bd5be1a3f2a0b55e856c5a5538cc61eb8d8b5223
ppsmrt <124156458+ppsmrt@users.noreply.github.com>
2025-08-14 21:13:45 +0530

    Delete assets/js/haeder.js

--- assets/js/haeder.js
+++
@@ -1,7 +0,0 @@
-document.getElementById("main-header").innerHTML = `
-<nav class="nav-bar">
-  <a href="index.html">Home</a>
-  <a href="posts.html">Latest Posts</a>
-  <a href="submit.html">Submit Post</a>
-</nav>
-`;
