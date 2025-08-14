5183973d28f01b03e34cbb5d30b4d7b8aaa8d1a8
ppsmrt <124156458+ppsmrt@users.noreply.github.com>
2025-08-14 20:22:53 +0530

    Update haeder.js

--- assets/js/haeder.js
+++ assets/js/haeder.js
@@ -1,1 +1,7 @@
-
+document.getElementById("main-header").innerHTML = `
+<nav class="nav-bar">
+  <a href="index.html">Home</a>
+  <a href="posts.html">Latest Posts</a>
+  <a href="submit.html">Submit Post</a>
+</nav>
+`;
